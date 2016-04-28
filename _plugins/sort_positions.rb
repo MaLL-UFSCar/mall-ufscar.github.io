b24ece332ed5ec3aab48a87aa05183439be0eedc
diorge <diorge.bs@gmail.com>
2016-04-28 20:16:24 -0300

    Added Postdoctoral position

--- _plugins/sort_positions.rb
+++ _plugins/sort_positions.rb
@@ -3,11 +3,14 @@
 module Jekyll
   module MemberSorting
     def sortmembers(members)
-       positions = ['Professor', 'PhD Student', 'MsC Student', 'Contributor', 'Undergraduate']
+       positions = ['Professor', 'Postdoctoral', 'PhD Student', 'MsC Student',
+                     'Undergraduate', 'Contributor']
        for member in members do
          member['posidx'] = positions.index(member['position'])
        end
-       return members.sort { |a, b| [a['posidx'], a['name']] <=> [b['posidx'], b['name']] }
+       return members.sort { |a, b|
+                        [a['posidx'], a['name']] <=> [b['posidx'], b['name']]
+                        }
     end
   end
 end
